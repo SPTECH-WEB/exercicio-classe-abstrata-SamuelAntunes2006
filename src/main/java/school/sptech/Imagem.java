148290f58e58f576428a1030c04100683b95ed76
SamuelAntunes2006 <samuel.asantos@sptech.school>
2025-10-31 13:32:45 -0300

    Atividade Finalizada

--- src/main/java/school/sptech/Imagem.java
+++ src/main/java/school/sptech/Imagem.java
@@ -1,4 +1,50 @@
 package school.sptech;
 
-public class Imagem {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Imagem extends Figura{
+private List<Figura> figuras = new ArrayList<>(7);
+
+    public void adicionar(Figura figura){
+
+            figuras.add(figura);
+
+
+    }
+    public Double calcularSomaDasAreas() {
+        Double somadetodasaslistas = 0.0;
+        for (Figura f : figuras) {
+            somadetodasaslistas += f.calcularArea();
+        }
+        return somadetodasaslistas;
+    }
+
+    public List<Figura>buscarPorAreaMaiorQue20(){
+        List<Figura> areaMaior = new ArrayList<>();
+        for(Figura f: figuras){
+            if(f.calcularArea() > 20){
+                areaMaior.add(f);
+            }
+        }
+        return areaMaior;
+    }
+
+    public List<Quadrado> buscarQuadrados() {
+        List<Quadrado> quadrados = new ArrayList<>();
+
+        for (Figura figura : figuras) {
+            if (figura instanceof Quadrado q) {
+                quadrados.add(q);
+            }
+        }
+        return quadrados;
+    }
+
+
+
+    @Override
+    public Double calcularArea() {
+        return 0.0;
+    }
 }
